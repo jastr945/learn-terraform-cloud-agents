644b216ef938d2d132f73cb67e119f232178af05
Polina Jastrzebska <polina.jastrzebska@hashicorp.com>
2023-03-03 08:51:43 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.image_id
   name  = "nginx"
   ports {
     internal = 80
